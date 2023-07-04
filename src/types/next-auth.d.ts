b02c24eef7d8c9aeb75784f9892b4e83fba5ca8f
Jordan Higuera Higuera <jordandark123@gmail.com>
2023-07-04 15:43:26 -0700

    types in next-auth sessions

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,18 +2,19 @@ import type {Session, User} from "next-auth"
 import type {JWT} from "next-auth/jwt"
 
 type UserId = string
+type Username = string | null
 
 declare module "next-auth/jwt" {
     interface JWT {
         id: UserId,
-        username?: string | null
+        username?: Username
     }
 }
 declare module "next-auth" {
     interface Session {
         user: User & {
             id: UserId,
-            username?: string | null
+            username?: Username
         }
     }
 }
